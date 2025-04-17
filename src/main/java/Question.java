bb93851f7454709e225e842e5ebe05b3711e477c
25gillp <25gillp@retsdstudent.ca>
2025-04-17 15:51:43 +0000

    Finished

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -2,5 +2,30 @@ import java.util.Scanner;
 
 
 public class Question {
-  
+    private String question;
+    public int totalMarks;
+    private String answer;
+    private String studentAnswer;
+
+    // constructor
+    public Question(String question, int totalMarks, String answer){
+        this.question = question;
+        this.totalMarks= totalMarks;
+        this.answer = answer;
+    }
+
+    public String AskQuestion(Scanner scanner){
+        System.out.println(this.question + " (" + this.totalMarks + " marks)");
+        this.studentAnswer = scanner.nextLine();
+
+        return this.studentAnswer;
+    }
+
+    public int CheckAnswer(){
+        if (this.studentAnswer.equals(this.answer)){
+            return this.totalMarks;
+        } else {
+            return 0;
+        }
+    }
 }
